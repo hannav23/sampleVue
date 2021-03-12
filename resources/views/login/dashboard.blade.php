b428db3cac014537ca1a4f130f247d32638396f3
Mark Han <markhanhernandez@gmail.com>
2021-03-12 12:49:08 +0800

    remove some codes

--- resources/views/login/dashboard.blade.php
+++ resources/views/login/dashboard.blade.php
@@ -1,14 +1,14 @@
 <div class="bgimg">
-    <div class="topleft">
-      <p>Logo</p>
-    </div>
+    {{-- <div class="topleft">
+     
+    </div> --}}
     <div class="middle">
       <h1>COMING SOON</h1>
       <hr class="hr">
-      <p>35 days left</p>
+      {{-- <p>35 days left</p> --}}
     </div>
     <div class="bottomleft">
-      <p>Some text</p>
+      {{-- <p>Some text</p> --}}
     </div>
   </div>
   
